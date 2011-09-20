6c0280adf42c5e9a1ff9876e45de35eb7102307f
brianfreytag <brianfreytag@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-09-20 02:32:17 +0000

    git-svn-id: https://plugins.svn.wordpress.org/social-media-widget/tags/2.9.3@440554 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- social-widget.php
+++ social-widget.php
@@ -1,8 +1,8 @@
 <?php
 /**
  * Plugin Name: Social Media Widget
- * Description: Adds links to all of your social media and sharing site profiles. Icons come in 3 sizes, 4 icon styles, and 4 animations.
- * Version: 2.9.4
+ * Description: Adds links to all of your social media and sharing site profiles. Tons of icons come in 3 sizes, 4 icon styles, and 4 animations.
+ * Version: 2.9.3
  * Author: Brian Freytag
  **/
 
